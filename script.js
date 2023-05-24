ac49d2e60442d05c705315bbbb4b0bfc5bae323c
Denys Shtoma <denusshtoma123@gmail.com>
2023-05-24 17:29:37 +0300

    Update 'Second update'

--- script.js
+++ script.js
@@ -9,13 +9,31 @@ private : false
 
 };
 
-const a = prompt("Один из последних просмотрених фильмов?" , ""),
-      b = prompt("На сколько оцените его?" , ""),
-      c = prompt("Один из последних просмотрених фильмов?" , ""),
-      d = prompt("На сколько оцените его?" , "");
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотрених фильмов?" , ""),
+          b = prompt("На сколько оцените его?" , "");
 
-personalMovieDB.movie[a] = b;
-personalMovieDB.movie[c] = d;
+          if(a != null && b != null && a != "" && b != "" && a.length < 50){
+            personalMovieDB.movie[a] = b;
+            console.log("done");
+          }
+          else {
+            console.log("error");
+            i--;
+          }
+          
+    
+}
+
+if(personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов"); 
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
+    console.log("Вы класический зритель"); 
+} else if(personalMovieDB.count >=30){
+    console.log("Заняться нехуй")
+} else{
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
